e75d4f42c7c3cc828bfdf0a45c4a405e0405a830
Eduardo Manoel <edu.manoel.p@gmail.com>
2018-06-02 10:35:00 -0300

    [FIX] Estrutura do projeto [NEW] Bootstrap [NEW] ng-view para tela de listageme formulário

---
+++ app/routes.js
@@ -0,0 +1,12 @@
+angular.module("myApp").config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "clientes/list.html"
+    })
+    .when("/list", {
+        templateUrl : "clientes/list.html"
+    })
+    .when("/form", {
+        templateUrl : "clientes/form.html"
+    })
+}]);
--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,36 @@ var concat = require('gulp-concat');
 var minify = require('gulp-minify');
 var rename = require('gulp-rename');
 
-gulp.task('vendor', function() {
-    var jsDest = './js/';
+function concatAndMinify(files, fileName, extension, filePath){
+    gulp.src(files)  
+        .pipe(concat(fileName))
+        .pipe(minify({ext: extension}))
+        .pipe(gulp.dest(filePath));
+}
 
-    return gulp.src(['./node_modules/angular/angular.js'])  
-        .pipe(concat('vendor.js'))
-        .pipe(gulp.dest(jsDest))
-        .pipe(minify({ext: '.min.js'}))
-        .pipe(gulp.dest(jsDest));
+gulp.task('vendor', function() {
+    var scripts = [
+        './node_modules/angular/angular.js',
+        './node_modules/angular-route/angular-route.js',
+        './node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js'
+    ];
+    concatAndMinify(scripts, 'vendor.js', '.min.js', './www/js/');
+
+    var styles = [
+        './node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css',
+        './node_modules/bootstrap/dist/css/bootstrap.css'
+    ];
+    concatAndMinify(styles, 'vendor.css', '.min.css', './www/css/');
+
+    var angularApp = [
+        './app/app.js',
+        './app/routes.js'
+    ];
+    concatAndMinify(angularApp, 'app.js', '.min.js', './www/js/');
 });
 
-gulp.task('default', ['vendor']);
+gulp.task('views', function() {
+    gulp.src('app/views/**/*').pipe(gulp.dest('./www/'));
+});
+
+gulp.task('default', ['vendor', 'views']);
