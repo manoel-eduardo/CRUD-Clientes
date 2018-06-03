9d7f4ff2af0a973b00bb7b114fb927157ff11dc3
Eduardo Manoel <edu.manoel.p@gmail.com>
2018-06-03 13:29:33 -0300

    [FIX] Input de telefones dinâmicos

--- app/controllers/clientes/clientes.controller.js
+++ app/controllers/clientes/clientes.controller.js
@@ -1,7 +1,21 @@
 angular.module("myApp").controller('ClientesController', ['$scope', function ($scope) {
     $scope.cliente = {
         "nome": '',
-        "cpf": ''
+        "cpf": '',
+        "dtNascimento": '',
+        "insertedAt": '',
+        "telefones": [{}, {}]
     };
 
+    $scope.addTelefone = function(){
+        $scope.cliente.telefones.push({});
+    }
+
+    $scope.removerTelefone = function(key){
+        $scope.cliente.telefones.splice(key, 1);
+    }
+
+    $scope.salvar = function(){
+        console.log($scope.cliente);
+    };
 }]);
